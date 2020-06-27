719c1d815f9c3cb79a979f7fd4db307e9d3f03fa
Cristian Cometti <cometticristian@gmail.com>
2020-06-27 13:22:27 -0300

    first part finished

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,18 +1,87 @@
 const db = require('../database/models');
+const { validationResult } = require('express-validator');
 const sequelize = db.sequelize;
 
 let moviesController = {
     list: function (req, res, next) {
         db.Movies.findAll()
-        .then((peliculas) => {
-            res.render('list', { peliculas: peliculas });
-        })
+            .then((peliculas) => {
+                res.render('list', { peliculas: peliculas });
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     },
     detail: function (req, res, next) {
         db.Movies.findByPk(req.params.id)
             .then((pelicula) => {
                 res.render('detail', { pelicula: pelicula })
             })
+            .catch((error) => {
+                console.log(error);
+            })
+    },
+    create: function (req, res, next) {
+        res.render('create');
+    },
+    store: function (req, res, next) {
+        let errors = validationResult(req);
+        if (errors.isEmpty()) {
+            db.Movies.create(req.body)
+                .then(() => {
+                    res.redirect('list');
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        } else {
+            res.render('create', { errors: errors.errors, datos: req.body })
+        }
+    },
+    edit: function (req, res, next) {
+        db.Movies.findByPk(req.params.id)
+            .then((pelicula) => {
+                res.render('edit', { pelicula: pelicula, id: req.params.id })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    },
+    update: function (req, res, next) {
+        let errors = validationResult(req);
+        if (errors.isEmpty()) {
+            db.Movies.update({
+                title: req.body.title,
+                rating: req.body.rating,
+                awards: req.body.awards,
+                length: req.body.length
+            }, {
+                where: {
+                    id: req.params.id
+                }
+            })
+                .then(() => {
+                    res.redirect('/movies/list')
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        } else {
+            res.render('edit', { errors: errors.errors, datos: req.body, id: req.params.id })
+        }
+    },
+    destroy: function (req, res, next) {
+        db.Movies.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(() => {
+            res.redirect('/movies/list');
+        })
+        .catch((error) => {
+            console.log(error);
+        })
     }
 }
 
--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var moviesController = require('../controllers/moviesController');
 var router = express.Router();
+const { check, validationResult, body } = require('express-validator');
 
 router.get('/list', moviesController.list);
 
 router.get('/detail/:id', moviesController.detail);
+
+router.get('/create', moviesController.create);
+
+router.post('/create', [
+    check('title').isLength({ min: 2, max:60 }).withMessage('El titulo de la pelicula debe tener entre 2 y 60 caracteres'),
+    check('rating').isNumeric().withMessage('Debe ingresar un numero'),
+    body('rating').custom((number) => {
+        return number >= 0 && number <= 10;
+    }).withMessage('El numero debe ser entre 0 y 10 inclusive'),
+    check('awards').isInt().withMessage('Debe ingresar un numero entero'),
+    body('awards').custom((number) => {
+        return number >= 0;
+    }).withMessage('El numero debe ser mayor o igual a 0'),
+    check('length').isInt().withMessage('Debe ingresar un numero entero'),
+    body('length').custom((number) => {
+        return number >= 0 && number <= 500;
+    }).withMessage('El numero debe ser entre 0 y 500 inclusive'),
+], moviesController.store);
+
+router.get('/edit/:id', moviesController.edit);
+
+router.put('/edit/:id', [
+    check('title').isLength({ min: 2, max:60 }).withMessage('El titulo de la pelicula debe tener entre 2 y 60 caracteres'),
+    check('rating').isNumeric().withMessage('Debe ingresar un numero'),
+    body('rating').custom((number) => {
+        return number >= 0 && number <= 10;
+    }).withMessage('El numero debe ser entre 0 y 10 inclusive'),
+    check('awards').isInt().withMessage('Debe ingresar un numero entero'),
+    body('awards').custom((number) => {
+        return number >= 0;
+    }).withMessage('El numero debe ser mayor o igual a 0'),
+    check('length').isInt().withMessage('Debe ingresar un numero entero'),
+    body('length').custom((number) => {
+        return number >= 0 && number <= 500;
+    }).withMessage('El numero debe ser entre 0 y 500 inclusive'),
+], moviesController.update);
+
+router.delete('/delete/:id', moviesController.destroy);
 
 module.exports = router;
